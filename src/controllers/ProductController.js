7a5bb06fa2e3fc874285c747516151549f57baba
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-06-28 22:17:47 -0300

    Criar rotas get

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,6 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
+router.get('/', async(req, res) => {
+    try{
+        const products = await Product.findAll();
+        
+        if(!products)
+            throw 'There are no registered products';
+        
+        res.status(200).send(products);
+    }catch(err){
+        res.status(404).send({error: `Query failed: ${err}`});
+    }
+});
+
+router.get('/:id', async(req, res) => {
+    let {id} = req.params;
+
+    try{
+        if(isNaN(id))
+            throw 'Id is not number';
+
+        const product = await Product.findOne({
+            where: {id}
+        });
+
+        if(!product)
+            throw 'Product not found';
+
+        res.status(200).send(product);
+    }catch(err){    
+        res.status(404).send({error: `Query failed: ${err}`});
+    }
+});
+
 router.post('/', async(req, res) => {
     let {name} = req.body;
 
